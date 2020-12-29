2a56961eaa23bc44a0416beb899dfba33d028db4
Huy Nguyen <huy.nguyen@trustingsocial.com>
2020-12-29 16:42:12 +0700

    feat: initiate router

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import { Provider as StyletronProvider, DebugEngine } from 'styletron-react'
+import { Client as Styletron } from 'styletron-engine-atomic'
+
+const engine = new Styletron()
+const debug = process.env.NODE_ENV === 'production' ? void 0 : new DebugEngine()
 
 ReactDOM.render(
-  <React.StrictMode>
+  <StyletronProvider value={engine} debug={debug} debugAfterHydration>
     <App />
-  </React.StrictMode>,
+  </StyletronProvider>,
   document.getElementById('root')
 )
 
---
+++ src/views/Home.js
@@ -0,0 +1,7 @@
+import { Button, KIND, SIZE, SHAPE } from 'baseui/button'
+
+function Home() {
+  return <Button>Hello world</Button>
+}
+
+export default Home
