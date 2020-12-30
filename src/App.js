79a6a74386fcad5d6031c727ea42e5e2acf30739
Huy Nguyen <huy.nguyen@trustingsocial.com>
2020-12-30 11:34:43 +0700

    fix(github-behaviour): use HashRouter instead of BrowseRouter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { HashRouter, Switch, Route, Link } from 'react-router-dom'
 import Home from './views/Home'
 import Login from './views/Login'
 import NotFound from './views/NotFound'
 
 function App() {
   return (
-    <Router>
+    <HashRouter hashType='hashbang'>
       <Switch>
         <Route exact path='/' component={Home}></Route>
         <Route exact path='/login' component={Login}></Route>
         <Route component={NotFound}></Route>
       </Switch>
-    </Router>
+    </HashRouter>
   )
 }
 
